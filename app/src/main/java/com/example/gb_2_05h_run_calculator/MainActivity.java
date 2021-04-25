fe2bd61fd14cdf57dbd6cf45256c5d7fbb383ba4
Vitaliy <v.a.grahov@ya.ru>
2021-04-26 02:56:41 +0500

    Запуск калькулятора через интент-фильтр

--- app/src/main/java/com/example/gb_2_05h_run_calculator/MainActivity.java
+++ app/src/main/java/com/example/gb_2_05h_run_calculator/MainActivity.java
@@ -2,7 +2,11 @@ package com.example.gb_2_05h_run_calculator;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button runCalc = findViewById(R.id.runCalc);
+        runCalc.setOnClickListener(new View.OnClickListener() {
+            @SuppressLint("QueryPermissionsNeeded")
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent("gb-calc://run");
+                if (intent.resolveActivity(getPackageManager()) != null) {
+                    startActivity(intent);
+                    finish();
+                }
+                runCalc.setText("Ошибка! Калькулятор не найден");
+            }
+        });
     }
 }
